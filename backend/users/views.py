88f7df57a0c3285d1d38d191e49446f7a1254840
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-18 22:19:21 +0300

    feat: User and UserProfile models

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User 
+from .serializers import UserSerializer
 
-# Create your views here.
+# User.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]

b03b8fb2cb54f3d3cf0bfd0e1f4001647aa385d9
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-20 12:48:45 +0300

    update: added media file attachments in serializers

--- backend/reports/serializers.py
+++ backend/reports/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
 from .models import Report, ReportMedia
-
-
-# Report Serializer
-class ReportSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Report
-        fields = ['id', 'reporter', 'title', 'description', 'status', 'location', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'reporter', 'status', 'created_at', 'updated_at']
+from media.models import Media
+from media.serializers import MediaSerializer
 
 
 # Report Media Serializer
@@ -16,7 +9,34 @@ class ReportMediaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ReportMedia
-        fields = ['id', 'report', 'media', 'uploaded_at']
-        read_only_fields = ['id', 'uploaded_at']
+        fields = ['id', 'report', 'media']
+
+
+# Report Serializer
+class ReportSerializer(serializers.ModelSerializer):
+    media_files = MediaSerializer(many=True, write_only=True, required=False)
+    attached_media = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Report
+        fields = ['id', 'reporter', 'title', 'description', 'status', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'reporter', 'status', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        media_files = validated_data.pop('media_files', [])
+        report = Report.objects.create(**validated_data, user=self.context['request'].user)
+
+        # Save media and a link to report
+        for media_data in media_files:
+            media = Media.objects.create(**media_data)
+            ReportMedia.objects.create(report=report, media=media)
+
+        return report
+    
+    def get_attached_media(self, obj):
+        report_media = ReportMedia.objects.filter(report=obj)
+        return MediaSerializer([rm.media for rm in report_media], many=True).data
+
+
 
         

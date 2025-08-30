118fa7557d35160017cb038cbab99ae2bda94426
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-08-30 22:54:42 +0300

    fix: ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,12 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "kijani-kinga.onrender.com",
+]
 
 AUTH_USER_MODEL = 'users.User'
 
